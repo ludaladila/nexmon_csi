e64cbf708a0c188263e1044f1d045658c507c43c
ludaladila <106161223+ludaladila@users.noreply.github.com>
2023-11-01 11:24:54 +0000

    install.sh

--- install.sh
+++ install.sh
@@ -75,11 +75,10 @@ make
 
 setStatus "Downloading Nexmon_CSI"
 cd $NEXDIR/patches/bcm43455c0/7_45_189/
-git clone https://github.com/zeroby0/nexmon_csi.git
+git clone https://github.com/seemoo-lab/nexmon_csi.git
 
 setStatus "Building and installing Nexmon_CSI"
 cd nexmon_csi
-git checkout pi-5.10.92
 make install-firmware
 
 setStatus "Installing makecsiparams"
